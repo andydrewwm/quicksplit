b63cb7b268ba9dc17040caa8b3f2b2cdfb3986e1
Ryan Amirthan Stephen <ryanstep@umich.edu>
2025-02-02 19:22:13 -0500

    added uuid gen for item id

--- backend/models.py
+++ backend/models.py
@@ -1,13 +1,18 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from typing import List, Optional
+from uuid import uuid4
+
+def generate_id():
+    return str(uuid4())
 
 class Item(BaseModel): #pydantics model automatically validates datatypes and converts JSON to python <-->
+    id: str = Field(default_factory=generate_id)
     name: str
     price: float
     assigned_to: Optional[str] = None  # Name of person
 
 class Receipt(BaseModel):
-    user_id: str
+    user_id: str #not using currently
     total_amount: float
     items: List[Item]
 
